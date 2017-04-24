e3ae2afb1ad1490ad289b125394822b035354ecf
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2017-04-24 02:11:57 -0400

    11
    1

--- models/rrhh_ccaf.py
+++ models/rrhh_ccaf.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models
 
 
 class rrhh_ccaf(models.Model):
-    _description = "Caja de Compensación de Asignación Familiar"
+    _description = "CCAF"
     _name = 'rrhh.ccaf'
     name = fields.Char('Glosa')
     codigo = fields.Integer('Codigo')
